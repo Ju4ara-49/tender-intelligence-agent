769ea989e90d962375895325c3fb64253ee7809b
Ju4ara-49 <mikrobond@gmail.com>
2026-09-11 14:23:12 +0300

    Install Telegram workflow UI

--- src/telegram_responsive.py
+++ src/telegram_responsive.py
@@ -11,6 +11,7 @@ from src.telegram_bot import HELP_TEXT
 from src.telegram_multiuser import MultiUserTelegramBot
 from src.telegram_profiles_runtime import install as install_profiles_runtime
 from src.telegram_profiles_ui import install as install_profiles_ui
+from src.telegram_workflow_ui import install as install_workflow_ui
 
 logger = logging.getLogger(__name__)
 
@@ -71,3 +72,4 @@ class ResponsiveMultiUserTelegramBot(MultiUserTelegramBot):
 
 install_profiles_ui(ResponsiveMultiUserTelegramBot)
 install_profiles_runtime(ResponsiveMultiUserTelegramBot)
+install_workflow_ui(ResponsiveMultiUserTelegramBot)
