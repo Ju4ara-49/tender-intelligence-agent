f5c561423b477fdbfe7c21fd883cc8eb1194128d
Ju4ara-49 <mikrobond@gmail.com>
2026-09-11 13:56:07 +0300

    Add document intelligence package

---
+++ src/documents/__init__.py
@@ -0,0 +1,5 @@
+"""Локальная обработка документов тендеров."""
+
+from src.documents.intelligence import DocumentHit, DocumentIntelligence, DocumentText
+
+__all__ = ["DocumentHit", "DocumentIntelligence", "DocumentText"]
