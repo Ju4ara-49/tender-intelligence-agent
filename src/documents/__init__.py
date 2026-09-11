a6febc5a121692014704b983f3ef9640e64873c3
Ju4ara-49 <mikrobond@gmail.com>
2026-09-11 14:16:02 +0300

    Export tender attachment document analyzer

--- src/documents/__init__.py
+++ src/documents/__init__.py
@@ -2,6 +2,7 @@
 
 from src.documents.downloader import DocumentDownloadError, DocumentDownloader, DownloadedDocument
 from src.documents.intelligence import DocumentHit, DocumentIntelligence, DocumentText, UnsafeArchiveError
+from src.documents.tender_attachments import TenderAttachment, TenderAttachmentAnalysis, TenderAttachmentAnalyzer
 
 __all__ = [
     "DocumentDownloadError",
@@ -10,5 +11,8 @@ __all__ = [
     "DocumentIntelligence",
     "DocumentText",
     "DownloadedDocument",
+    "TenderAttachment",
+    "TenderAttachmentAnalysis",
+    "TenderAttachmentAnalyzer",
     "UnsafeArchiveError",
 ]
