afa81056c02f21f15c533b25f6659a37bef3ae44
Ju4ara-49 <mikrobond@gmail.com>
2026-09-11 13:58:05 +0300

    Export safe document downloader

--- src/documents/__init__.py
+++ src/documents/__init__.py
@@ -1,5 +1,14 @@
 """Локальная обработка документов тендеров."""
 
-from src.documents.intelligence import DocumentHit, DocumentIntelligence, DocumentText
+from src.documents.downloader import DocumentDownloadError, DocumentDownloader, DownloadedDocument
+from src.documents.intelligence import DocumentHit, DocumentIntelligence, DocumentText, UnsafeArchiveError
 
-__all__ = ["DocumentHit", "DocumentIntelligence", "DocumentText"]
+__all__ = [
+    "DocumentDownloadError",
+    "DocumentDownloader",
+    "DocumentHit",
+    "DocumentIntelligence",
+    "DocumentText",
+    "DownloadedDocument",
+    "UnsafeArchiveError",
+]
