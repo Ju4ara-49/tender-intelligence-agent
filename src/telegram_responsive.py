019e263c8b7e89e38e87474fd7401e9316a88c27
Ju4ara-49 <mikrobond@gmail.com>
2026-09-11 13:55:15 +0300

    Integrate Telegram saved-search UI

--- src/telegram_responsive.py
+++ src/telegram_responsive.py
@@ -9,17 +9,13 @@ import httpx
 
 from src.telegram_bot import HELP_TEXT
 from src.telegram_multiuser import MultiUserTelegramBot
+from src.telegram_profiles_ui import install as install_profiles_ui
 
 logger = logging.getLogger(__name__)
 
 
 class ResponsiveMultiUserTelegramBot(MultiUserTelegramBot):
-    """Multi-user bot with interruptible short polling.
-
-    Площадки и пользовательские критерии берутся из базового TelegramBot;
-    этот класс отвечает только за сетевой polling и не делает monkey-patch
-    глобальных словарей.
-    """
+    """Multi-user bot with interruptible short polling."""
 
     POLL_TIMEOUT_SECONDS = 2
     REQUEST_TIMEOUT_SECONDS = 15
@@ -70,3 +66,6 @@ class ResponsiveMultiUserTelegramBot(MultiUserTelegramBot):
                 self._handle_callback(update["callback_query"])
             elif update.get("message"):
                 self._handle_message(update["message"])
+
+
+install_profiles_ui(ResponsiveMultiUserTelegramBot)
