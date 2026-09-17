438d1f37a52626f2fedacbf117356d623cff8bad
Ju4ara-49 <mikrobond@gmail.com>
2026-09-18 00:48:45 +0300

    risk_init.py

---
+++ risk_init.py
@@ -0,0 +1,4 @@
+"""Детерминированный (без LLM) движок оценки риска тендера."""
+from src.risk.engine import RiskAssessment, RiskEngine, RiskFactor
+
+__all__ = ["RiskAssessment", "RiskEngine", "RiskFactor"]
