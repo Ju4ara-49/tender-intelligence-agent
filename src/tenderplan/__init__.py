72ec61eb23b38c9f2b86daf9753fa6551f4dcc4c
Ju4ara-49 <mikrobond@gmail.com>
2026-09-18 01:02:12 +0300

    feat(tenderplan): expose task persistence adapter

--- src/tenderplan/__init__.py
+++ src/tenderplan/__init__.py
@@ -1,5 +1,6 @@
 """TenderPlan-inspired domain capabilities."""
 
+from .storage import TenderTaskStore
 from .tasks import TaskPriority, TaskStatus, TenderTask
 
-__all__ = ["TaskPriority", "TaskStatus", "TenderTask"]
+__all__ = ["TaskPriority", "TaskStatus", "TenderTask", "TenderTaskStore"]
