4877fd5609f7e0ad0799da986d513a17ac6b3f17
Ju4ara-49 <mikrobond@gmail.com>
2026-09-18 01:03:01 +0300

    feat(tenderplan): expose application task service

--- src/tenderplan/__init__.py
+++ src/tenderplan/__init__.py
@@ -1,6 +1,14 @@
 """TenderPlan-inspired domain capabilities."""
 
+from .service import application_task_id, ensure_application_task
 from .storage import TenderTaskStore
 from .tasks import TaskPriority, TaskStatus, TenderTask
 
-__all__ = ["TaskPriority", "TaskStatus", "TenderTask", "TenderTaskStore"]
+__all__ = [
+    "TaskPriority",
+    "TaskStatus",
+    "TenderTask",
+    "TenderTaskStore",
+    "application_task_id",
+    "ensure_application_task",
+]
