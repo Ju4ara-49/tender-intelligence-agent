38a67857bc4dff8ac947cf60bee1b797c9645964
Ju4ara-49 <mikrobond@gmail.com>
2026-09-18 12:15:52 +0300

    feat(tenderplan): export document foundation

--- src/tenderplan/__init__.py
+++ src/tenderplan/__init__.py
@@ -1,10 +1,10 @@
 """TenderPlan-inspired domain capabilities."""
 
-from .lifecycle import (\n    ALLOWED_TRANSITIONS,\n    CRM_TO_LIFECYCLE,\n    TERMINAL_STATUSES,\n    InvalidLifecycleTransition,\n    TenderLifecycleStatus,\n    can_transition,\n    lifecycle_from_crm,\n    transition,\n)\nfrom .service import application_task_id, ensure_application_task, task_priority_for_deadline\nfrom .storage import TenderTaskStore
+from .documents import DocumentExtractionStatus, TenderDocument, TenderDocumentStore, content_sha256\nfrom .lifecycle import (\n    ALLOWED_TRANSITIONS,\n    CRM_TO_LIFECYCLE,\n    TERMINAL_STATUSES,\n    InvalidLifecycleTransition,\n    TenderLifecycleStatus,\n    can_transition,\n    lifecycle_from_crm,\n    transition,\n)\nfrom .service import application_task_id, ensure_application_task, task_priority_for_deadline\nfrom .storage import TenderTaskStore
 from .tasks import TaskPriority, TaskStatus, TenderTask
 
 __all__ = [
-    "TenderLifecycleStatus",\n    "InvalidLifecycleTransition",\n    "TERMINAL_STATUSES",\n    "ALLOWED_TRANSITIONS",\n    "CRM_TO_LIFECYCLE",\n    "can_transition",\n    "transition",\n    "lifecycle_from_crm",\n    "TaskPriority",
+    "TenderDocument",\n    "TenderDocumentStore",\n    "DocumentExtractionStatus",\n    "content_sha256",\n    "TenderLifecycleStatus",\n    "InvalidLifecycleTransition",\n    "TERMINAL_STATUSES",\n    "ALLOWED_TRANSITIONS",\n    "CRM_TO_LIFECYCLE",\n    "can_transition",\n    "transition",\n    "lifecycle_from_crm",\n    "TaskPriority",
     "TaskStatus",
     "TenderTask",
     "TenderTaskStore",
