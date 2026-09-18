9bafa946c92adeab72f84edbcb9b069eac86c55d
Ju4ara-49 <mikrobond@gmail.com>
2026-09-18 13:28:48 +0300

    test(rosatom): preserve direct result links for detail parsing

--- tests/test_tenderplan_fabrikant_rosatom_documents.py
+++ tests/test_tenderplan_fabrikant_rosatom_documents.py
@@ -17,3 +17,14 @@ def test_rosatom_detail_document_contract():
     docs=RosatomCollector._extract_documents(BeautifulSoup(HTML,"lxml"),"https://zakupki.rosatom.ru/proc/1")
     assert len(docs)==2
     assert docs[1]["filename"]=="contract.docx"
+
+def test_rosatom_results_keep_direct_row_link_for_detail_loading():
+    html = """<table>
+    <tr><th>Номер закупки</th><th>Предмет договора</th><th>НМЦ, руб</th></tr>
+    <tr><td>123456 (789)</td><td>Поставка запасных частей</td><td>100000</td></tr>
+    </table>"""
+    collector = RosatomCollector({})
+    results = collector._parse_results(html)
+    assert len(results) == 1
+    assert results[0].url == "https://zakupki.rosatom.ru/procurements/123456"
+    assert collector._urls["123456"] == results[0].url
